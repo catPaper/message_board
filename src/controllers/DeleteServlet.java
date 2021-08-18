ab2fdc5dea7af74ac96f66a1b9c6c2d2b9a82eec
ryouta.osada <cat.3210sm@gmail.com>
2021-08-18 17:03:28 +0900

    DeleteServletの作成

---
+++ src/controllers/DeleteServlet.java
@@ -0,0 +1,56 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Message;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class DeleteServlet
+ */
+@WebServlet("/destroy")
+public class DeleteServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            //セッションスコープからメッセージIDを取得して
+            //該当のIDのメッセージのみをデータベースから取得
+            Message m = em.find(Message.class, request.getSession().getAttribute("message_id"));
+
+            //データベースから該当のメッセージを削除
+            em.getTransaction().begin();
+            em.remove(m);
+            em.getTransaction().commit();
+            em.close();
+
+            //セッションスコープから不要になったデータを削除
+            request.getSession().removeAttribute("message_id");
+
+            //indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
+    }
+
+}
